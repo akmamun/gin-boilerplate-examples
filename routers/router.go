b4c13b2e3c14cbaef6b95f492b80b2a1b260e319
mohammad.mamun <mohammad.mamun@upaybd.com>
2022-05-19 16:55:57 +0600

    refactoring projects same as boilerplate

---
+++ routers/router.go
@@ -0,0 +1,29 @@
+package routers
+
+import (
+	"github.com/akmamun/gin-boilerplate-examples/routers/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+func Routes(db *gorm.DB) *gin.Engine {
+
+	environment := viper.GetBool("DEBUG")
+	if environment {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	allowedHosts := viper.GetString("ALLOWED_HOSTS")
+	router := gin.New()
+	router.SetTrustedProxies([]string{allowedHosts})
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(middleware.CORSMiddleware())
+
+	RegisterRoutes(router, db) //routes register
+
+	return router
+}
